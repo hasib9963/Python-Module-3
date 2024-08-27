6fe87813baaa9f2dab6a9979993ad813c9ade263
Hasib Mohammad Mahede Hasan <hasib9963@gmail.com>
2024-08-27 08:04:38 +0600

    string in python

---
+++ explore_string.py
@@ -0,0 +1,11 @@
+name = 'Sakib\'s Khan' #escape
+name2 = "Sakib's Khan"
+name3 = """
+    Sakib khan
+    number one
+"""
+
+print(name)
+# string means is a sequence of character
+# mutable means changeable
+# immutable means you can not change it
